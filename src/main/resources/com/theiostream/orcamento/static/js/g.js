4192932cfbedb5f04a9adc1426022a4dc8fae277
Daniel Ferreira <daniel.calibeta@gmail.com>
2015-02-03 16:30:58 -0200

    Implement switching between nominal and inflation-corrected values in historical series.

--- src/main/resources/com/theiostream/orcamento/static/js/g.js
+++ src/main/resources/com/theiostream/orcamento/static/js/g.js
@@ -425,6 +425,14 @@ function addProgramaSelector() {
 
 /* History {{{ */
 
+var translation = {
+	"loa": "LOA",
+	"pago": "Pago",
+	"aumloa": "Aumento (LOA)",
+	"aumpago": "Aumento (Pago)",
+	"inf": "Inflação (IPCA)"
+};
+
 function createGraphHistory(id, tit) {
 	var row = document.createElement("div");
 	row.setAttribute("class", "row");
@@ -448,16 +456,46 @@ function createGraphHistory(id, tit) {
 }
 
 function reloadDataHistory(id, rinfo) {
-	var translation = {
-		"loa": "LOA",
-		"pago": "Pago",
-		"aumloa": "Aumento (LOA)",
-		"aumpago": "Aumento (Pago)",
-		"inf": "Inflação (IPCA)"
-	};
-
 	var el = document.getElementById(id + "-history");
 	
+	d3.json(documentURL() + "/d?rinfo=" + rinfo, function(error, data) {
+		data.sort(function(a,b){ return parseInt(a.letter)>parseInt(b.letter)?1:parseInt(a.letter)<parseInt(b.letter)?-1:0; });
+	
+		var ageNames = d3.keys(data[0]).filter(function(key) { return key=="loa" || key=="pago"; });
+		data.forEach(function(d) {
+			d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+		});
+		reloadHistory_(id, el, data, ageNames);
+		
+		d3.selectAll(document.getElementById(id + "-group").children).on('click', function() {
+			if (!this.classList.contains("active")) {
+				var siblings = this.parentNode.children;
+				for (var i=0; i<siblings.length; i++) {
+					if (siblings[i].classList.contains("active")) {
+						siblings[i].classList.remove("active");
+						break;
+					}
+				}
+				this.classList.add("active");
+				
+				var btnkey = this.getAttribute("data-key");
+				var ageNames = d3.keys(data[0]).filter(function(key) {
+					if (btnkey == "noinf"){console.log("WOOT"); return key=="loa" || key=="pago";}
+					return key=="infloa" || key=="infpago";
+				});
+				console.log(ageNames);
+				data.forEach(function(d) {
+					d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+				});
+
+				el.innerHTML = "";
+				reloadHistory_(id, el, data, ageNames);
+			}
+		});
+	});
+}
+
+function reloadHistory_(id, el, data, ageNames) {
 	var margin = {top: 20, right: 20, bottom: 50, left: 40};
 	var width = el.offsetWidth - margin.left - margin.right;
 	var height = el.offsetHeight - margin.top - margin.bottom;
@@ -483,92 +521,66 @@ function reloadDataHistory(id, rinfo) {
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	d3.json(documentURL() + "/d?rinfo=" + rinfo, function(error, data) {
-		data.sort(function(a,b){ return parseInt(a.letter)>parseInt(b.letter)?1:parseInt(a.letter)<parseInt(b.letter)?-1:0; });
-		var ageNames = d3.keys(data[0]).filter(function(key) { return key=="loa" || key=="pago"; });
+	x0.domain(data.map(function(d) { return d.letter; }));
+	x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+	y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
 
-		data.forEach(function(d) {
-			d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-		});
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
 
-		x0.domain(data.map(function(d) { return d.letter; }));
-		x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-		y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Valor");
 
-		svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
+	var state = svg.selectAll(".state")
+		.data(data)
+		.enter().append("g")
+		.attr("class", "g")
+		.attr("transform", function(d) { return "translate(" + x0(d.letter) + ",0)"; });
 
-		svg.append("g")
-			.attr("class", "y axis")
-			.call(yAxis)
-			.append("text")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", ".71em")
-			.style("text-anchor", "end")
-			.text("Valor");
+	state.selectAll("rect")
+		.data(function(d) { return d.ages; })
+		.enter().append("rect")
+		.attr("width", x1.rangeBand())
+		.attr("x", function(d) { return x1(d.name); })
+		.attr("y", function(d) { return y(d.value); })
+		.attr("height", function(d) { return height - y(d.value); })
+		.style("fill", function(d) { return color(d.name); });
 
-		var state = svg.selectAll(".state")
-			.data(data)
-			.enter().append("g")
-			.attr("class", "g")
-			.attr("transform", function(d) { return "translate(" + x0(d.letter) + ",0)"; });
+	var legend = svg.selectAll(".legend")
+		.data(ageNames.slice().reverse())
+		.enter().append("g")
+		.attr("class", "legend")
+		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-		state.selectAll("rect")
-			.data(function(d) { return d.ages; })
-			.enter().append("rect")
-			.attr("width", x1.rangeBand())
-			.attr("x", function(d) { return x1(d.name); })
-			.attr("y", function(d) { return y(d.value); })
-			.attr("height", function(d) { return height - y(d.value); })
-			.style("fill", function(d) { console.log("name is " + d.name); return color(d.name); });
+	legend.append("rect")
+		.attr("x", width - 18)
+		.attr("width", 18)
+		.attr("height", 18)
+		.style("fill", color);
 
-		var legend = svg.selectAll(".legend")
-			.data(ageNames.slice().reverse())
-			.enter().append("g")
-			.attr("class", "legend")
-			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-		legend.append("rect")
-			.attr("x", width - 18)
-			.attr("width", 18)
-			.attr("height", 18)
-			.style("fill", color);
-
-		legend.append("text")
-			.attr("x", width - 24)
-			.attr("y", 9)
-			.attr("dy", ".35em")
-			.style("text-anchor", "end")
-			.style("font-size", "10px")
-			.text(function(d) { return translation[d]; });
-		
-		svg.append("text")
-			.style("font-size", "10px")
-			.style("text-align", "center")
-			.attr("width", width)
-			.attr("y", height + 30)
-			.text("Nota: Correções inflacionárias (IPCA-E) de valores da são realizadas entre janeiro do ano referido e " + ipca + " (mais recente divulgação do índice).");
-
-		d3.selectAll(document.getElementById(id + "-group").children).on('click', function() {
-			if (!this.classList.contains("active")) {
-				var siblings = this.parentNode.children;
-				for (var i=0; i<siblings.length; i++) {
-					if (siblings[i].classList.contains("active")) {
-						siblings[i].classList.remove("active");
-						break;
-					}
-				}
-				
-				this.classList.add("active");
-				
-				var key = this.getAttribute("data-key");
-				// TODO IMPLEMENT
-			}
-		});
-	});
+	legend.append("text")
+		.attr("x", width - 24)
+		.attr("y", 9)
+		.attr("dy", ".35em")
+		.style("text-anchor", "end")
+		.style("font-size", "10px")
+		.text(function(d) { return translation[d]; });
+	
+	svg.append("text")
+		.style("font-size", "10px")
+		.style("text-align", "center")
+		.attr("width", width)
+		.attr("y", height + 30)
+		.text("Nota: Correções inflacionárias (IPCA-E) de valores da são realizadas entre janeiro do ano referido e " + ipca + " (mais recente divulgação do índice).");
 }
 
 /* }}} */
